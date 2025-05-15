af8e73e576a1cbe9588b641cc5feef02ce2244da
Merchok <maxvsalien@gmail.com>
2025-05-15 14:13:22 +0300

    Sign up is now working propely

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 import json
 import hashlib
 from flask import Flask, render_template, request, redirect, url_for
+import time
 
 
 app = Flask(__name__)
@@ -23,7 +24,7 @@ def login():
         if name in users and users[name]["password"] == password_hash:
             return redirect(url_for("home", username=name))
         else:
-            return "Неверное имя или пароль!"
+            return "Wrong name or password!"
 
     return render_template("login.html")
 
@@ -34,7 +35,39 @@ def home(username):
         balance = user["balance"]
         return render_template("home.html", name=username, balance=round(balance, 2))
     else:
-        return "Пользователь не найден."
+        return "User not found."
+    
+@app.route("/signup_success")
+def signup_success():
+    return render_template("signup_success.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+
+
+        if name in users:
+            return "User already exist!"
+
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        users[name] = {
+            "password": password_hash,
+            "balance": 15,
+            "trans": [],
+            "stocks": {}
+        }
+
+        with open("Users.json", "w") as f:
+            json.dump(users, f, indent=4)
+
+        return render_template("signup_success.html")
+
+    
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
